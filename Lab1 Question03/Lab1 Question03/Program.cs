53e50330eaed2f11f46c5c40f9904866989ff056
chamo <137000540+chamo2002@users.noreply.github.com>
2024-04-10 09:58:09 +0530

    Add files via upload

---
+++ Lab1 Question03/Lab1 Question03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Question03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int sum = 0;
+
+ 5 Console.Write("Enter a number:");
+   num=Convert.ToInt32(Console.ReadLine());
+
+   {
+       if (num <= 0)
+       {
+           Console.WriteLine("ERROR");
+       }
+       else
+       {
+           for (int i = 1; i <= num; i++)
+           {
+               sum += i;
+           }
+       }
+   }
+   Console.WriteLine("The Sum:" + sum);
+   Console.ReadLine();
+        }
+    }
+}
