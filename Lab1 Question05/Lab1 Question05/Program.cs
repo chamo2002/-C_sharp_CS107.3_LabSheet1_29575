f6b1ad9f12e7a227bc8258a1877222f3eda53c26
chamo <137000540+chamo2002@users.noreply.github.com>
2024-04-10 10:12:24 +0530

    Add files via upload

---
+++ Lab1 Question05/Lab1 Question05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Question05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Multiplication Table for " + number + ":");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(number + " x " + i + " = " + (number * i));
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
