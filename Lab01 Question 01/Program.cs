b75c9f8e6492bd362db017fd8720d930d80dae90
chamo <137000540+chamo2002@users.noreply.github.com>
2024-04-10 09:46:22 +0530

    Add files via upload

---
+++ Lab01 Question 01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01_Question_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length;
+            int width;
+
+            Console.Write("Enter the length:");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the width:");
+            width = Convert.ToInt32(Console.ReadLine());
+            int area = length * width;
+            Console.WriteLine("Area:" + area);
+            Console.ReadLine();
+        }
+    }
+}
