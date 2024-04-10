c1b347a988914ee04c510687924779ee57d7547f
chamo <137000540+chamo2002@users.noreply.github.com>
2024-04-10 09:53:56 +0530

    Add files via upload

---
+++ Lab1 Question02/Lab1 Question02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Question02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter the number:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("This is an even number");
+            }
+            else 
+            {
+                Console.WriteLine("This is an odd number");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
